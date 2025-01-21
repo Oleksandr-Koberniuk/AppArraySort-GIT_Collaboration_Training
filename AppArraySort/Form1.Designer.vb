2aecfb62486ac28c3946a4dc17ed7af70f29e7a4
20241982 <20241982@dfei.ie>
2025-01-21 14:40:40 +0000

    Add project files.

---
+++ AppArraySort/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSort = New System.Windows.Forms.Button()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnSort
+        '
+        Me.btnSort.Location = New System.Drawing.Point(183, 209)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(162, 51)
+        Me.btnSort.TabIndex = 0
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 25
+        Me.lstDisplay.Location = New System.Drawing.Point(183, 61)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(162, 129)
+        Me.lstDisplay.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Controls.Add(Me.btnSort)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSort As Button
+    Friend WithEvents lstDisplay As ListBox
+End Class
---
+++ AppArraySort/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+        'lstDisplay.Items.Clear()
+
+        Dim intNumbers() As Integer = {5, 2, 4, 1, 0}
+        For xx = intNumbers.GetLowerBound(0) To intNumbers.GetUpperBound(0) - 1
+            For i = intNumbers.GetLowerBound(0) To intNumbers.GetUpperBound(0) - 1
+                If (intNumbers(i) > intNumbers(i + 1)) Then
+                    Dim exc As Integer = intNumbers(i)
+                    intNumbers(i) = intNumbers(i + 1)
+                    intNumbers(i + 1) = exc
+                End If
+            Next i
+        Next xx
+
+        For i = intNumbers.GetLowerBound(0) To intNumbers.GetUpperBound(0)
+            lstDisplay.Items.Add(intNumbers(i))
+        Next
+    End Sub
+End Class
